d0eab3939d410b5fd7b2df27716908a29e43a70b
HoussemAmara <houssemamara35@outlook.fr>
2023-10-22 10:42:30 +0200

    first commit

---
+++ app/[searchTerm]/error.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div className="error-container">
+            <h1>Oops! An Error Occurred</h1>
+            <p>We're sorry, but something went wrong. Please try again later.</p>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ app/[searchTerm]/loading.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+const loading = () => {
+    return (
+        <h2>loading ...</h2>
+    )
+}
+
+export default loading
